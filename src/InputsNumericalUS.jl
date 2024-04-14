b6220cdbc3c75ac14de44d33e28025263793153f
Peter Tkatchenko <peteratkatchenko@gmail.com>
2024-04-14 14:28:43 +0200

    Updating US script

--- src/InputsNumericalUS.jl
+++ src/InputsNumericalUS.jl
@@ -2,7 +2,7 @@ using StatFiles
 using DataFrames 
 using Econometrics 
 using CSV 
-using RCall
+
 
 #PARAMETER VALUES FROM MCGRATTAN
 psi=1.4841
@@ -52,53 +52,91 @@ dropmissing!(data, Cols(r"laborwedge.*"))
 
 data = select(data, r"pop.*", r"laborwedge.*", r"efficiencywedge.*", r"medianrent.*", r"gdp.*", :fips, :msaname, :statename)
 
+data1 = stack(data, Cols(r"pop.*"), [:fips, :msaname, :statename], variable_name=:year, value_name=:pop)
+data1.year = parse.(Int, replace.(data1.year, r".*_" => ""))
+data1 = sort(data1, [:fips, :year])
+
+data2 = stack(data, Cols(r"laborwedge.*"), [:fips, :msaname, :statename], variable_name=:year, value_name=:laborwedge)
+data2.year = parse.(Int, replace.(data2.year, r".*_" => ""))
+data2 = sort(data2, [:fips, :year])
+
+data3 = stack(data, Cols(r"efficiencywedge.*"), [:fips, :msaname, :statename], variable_name=:year, value_name=:efficiencywedge)
+data3.year = parse.(Int, replace.(data3.year, r".*_" => ""))
+data3 = sort(data3, [:fips, :year])
+
+data4 = stack(data, Cols(r"medianrent.*"), [:fips, :msaname, :statename], variable_name=:year, value_name=:medianrent)
+data4.year = parse.(Int, replace.(data4.year, r".*_" => ""))
+data4 = sort(data4, [:fips, :year])
+
+data5 = stack(data, Cols(r"gdp.*"), [:fips, :msaname, :statename], variable_name=:year, value_name=:gdp)
+data5.year = parse.(Int, replace.(data5.year, r".*_" => ""))
+data5 = sort(data5, [:fips, :year])
+
+data = hcat(data1, data2, data3, data4, data5, makeunique=true)
+
+data = select(data, [:fips, :msaname, :statename, :year, :pop, :laborwedge, :efficiencywedge, :medianrent, :gdp])
+
+#CREATE TIME DUMMIES
+
+for i in 2005:2008
+	data[!, "dummy$i"] = zeros(Int, length(data.year))
+	for j in eachindex(data.year)
+		if data[j, :year] == i
+			data[j, "dummy$i"] = 1
+		end 
+	end 
+end 
+
+
+#CREATE LOG VARIABLES 
+
+data.logpop = log.(data[!, :pop])
+
+data.logmedianrent = log.(data[!, :medianrent])
+
+data.logeff = log.(data[!, :efficiencywedge])
+
+#Important: labor wedge is now defined as log of tau 
+
+data.loglaborwedge = log.(1 .- data[!, :laborwedge])
+
+
+#DROP VARIABLES THAT ARE MISSING
+
+dropmissing!(data, [:logpop, :logeff, :loglaborwedge])
 
 
 
-reshape long pop laborwedge efficiencywedge medianrent gdp, i(fips) j(year)
+#USE EQUATION (20) TO COMPUTE LOG OF EXCESSIVE FRICTIONS AND ALPHA_5 AND THEN DETERMINE KAPPA
+
+data.loglaborpop = data[!, :loglaborwedge] .- 0.5 .* log.(exp.(data[!, :logpop]) .* 1000)
+
+model = fit(EconometricModel, @formula(loglaborpop ~ dummy2005 + dummy2006 + dummy2007), data)
+
+alpha5 = coef(model)[1]
+
+alpha5dummy2005 = alpha5 + coef(model)[2]
+
+alpha5dum2006 = alpha5 + coef(model)[3]
+
+alpha5dum2007 = alpha5 + coef(model)[4]
+
+kappa = exp(alpha5 - log(2/3) + (1/2)*log(3.1415))
+
+kappa2005 = exp(alpha5dum2005 - log(2/3) + (1/2)*log(3.1415))
+
+kappa2006 = exp(alpha5dum2006 - log(2/3) + (1/2)*log(3.1415))
+
+kappa2007 = exp(alpha5dum2007 - log(2/3) + (1/2)*log(3.1415))
+
+println(kappa)
+
+kappa2 = round(kappa, 0.00001)
+
+logexcfrict = log.( exp.(data[!, :loglaborwedge]) ./ kappa2 * (3/2) * (3.1415/(data[!, :pop] .*1000)) .^0.5)
 
 
-* CREATE TIME DUMMIES
-forvalues x=2005/2008 {
-	gen dummy`x'=0
-	replace dummy`x'=1 if year==`x' 
-}
-
-* CREATE LOG VARIABLES 
-gen logpop=log(pop)
-gen logmedianrent=log(medianrent)
-gen logeff=log(efficiencywedge)
-* Important: labor wedge is now defined as log of tau 
-gen loglaborwedge=log(1-laborwedge)
-
-
-* DROP VARIABLES THAT ARE MISSING
-drop if logpop==.
-drop if logeff==.
-drop if loglaborwedge==.
-
-log on
-
-* USE EQUATION (20) TO COMPUTE LOG OF EXCESSIVE FRICTIONS AND ALPHA_5 AND THEN DETERMINE KAPPA
-gen loglaborpop=loglaborwedge-0.5*log(exp(logpop)*1000)
-reg loglaborpop dummy2005 dummy2006 dummy2007
-scalar alpha5=_b[_cons]
-scalar alpha5dum2005=_b[_cons]+_b[dummy2005]
-scalar alpha5dum2006=_b[_cons]+_b[dummy2006]
-scalar alpha5dum2007=_b[_cons]+_b[dummy2007]
-
-scalar kappa=exp(alpha5-log(2/3)+1/2*log(3.1415))
-scalar kappa2005=exp(alpha5dum2005-log(2/3)+1/2*log(3.1415))
-scalar kappa2006=exp(alpha5dum2006-log(2/3)+1/2*log(3.1415))
-scalar kappa2007=exp(alpha5dum2007-log(2/3)+1/2*log(3.1415))
-
-display kappa
-scalar kappa2=round(kappa, 0.00001)
-
-gen logexcfrict=log(exp(loglaborwedge)/kappa2*3/2*(3.1415/(pop*1000))^0.5)
-
-
-* KEEP VARIABLES NEEDED TO RUN COUNTERFACTUAL EXERCISE
+#KEEP VARIABLES NEEDED TO RUN COUNTERFACTUAL EXERCISE
 keep year fips msaname statename logeff logexcfrict pop
 
 reshape wide logeff logexcfrict pop, i(fips) j(year)
